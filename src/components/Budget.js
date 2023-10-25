2801e7698f6b0104488b413ae0f480b45e6450fc
juanosorio1989 <jasebvideoyweb@gmail.com>
2023-10-24 21:12:52 -0400

    optionCurrency

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,14 +2,27 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { budget, remaining } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+        
+        if(newBudget > remaining) {
+            alert("The value cannot exceed remaining funds  £"+remaining);
+            setNewBudget(remaining);
+           return;
+        }else if(newBudget < totalExpenses){
+            alert("You cannot reduce the budget value lower than the spending £");
+        }else{
+            setNewBudget(event.target.value);
+        }
     }
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budet: £{budget}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
 </div>
     );
