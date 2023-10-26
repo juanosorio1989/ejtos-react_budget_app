624e13725ff842f0c12c50f1c7bb9aa986b80e54
juanosorio1989 <jasebvideoyweb@gmail.com>
2023-10-25 23:21:49 -0400

    avanceCurrency

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -5,18 +5,19 @@ const Currency = (props) => {
    const { currencies } = useContext(AppContext);
 
     const [name, setName] = useState('');
-    const [icon, setIcon] = useState('');
+    //const [icon, setIcon] = useState('');
    // const [cost, setCost] = useState('');
    // const [action, setAction] = useState('');
 
-   {/* const submitEvent = () => {
+   /* const submitEvent = () => {
 
 
         const expense = {
             name: name,
             cost: parseInt(cost),
         };
-    }; */}
+    }; */
+
 
     return (
         <div>
@@ -25,14 +26,14 @@ const Currency = (props) => {
             <div className="input-group mb-3" style={{ marginLeft: '2rem', color:'white', backgroundColor:'green'}}>
                     <div className="input-group-prepend" >
                 <label className="input-group-text" htmlFor="inputGroupSelect01" style={{color:'white', backgroundColor:'green'}}>Currency(£ Pound)</label>
-                  
-                  <select style={{color:'white', backgroundColor:'green'}} className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
-                        <option defaultValue>{name}</option>
+                </div>
+                  <select style={{color:'white', width: '100%', backgroundColor:'green'}} className="custom-select" id="inputGroupSelect01" onChange={(event) => setName(event.target.value)}>
+                        <option defaultValue style={{color:'whitesmoke'}}>{name}</option>
                         {currencies.map((currency) => (
-                       <option value={currency.value} ></option>
+                       <option value={currency.value} style={{color:'whitesmoke'}}>{currency.name}</option>
             ))}
                   </select>
-                  </div>
+                  
                  
                 </div>
                 </div>
